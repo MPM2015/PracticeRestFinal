f3b0e247545b1bd220c7370d10cbb88c092fd799
Monisha <chatwithmaheshmp@gmail.com>
2022-01-26 14:56:01 +0530

    last change by harish

--- src/test/java/com/spotify/oauth2/utility/Routes.java
+++ src/test/java/com/spotify/oauth2/utility/Routes.java
@@ -8,5 +8,5 @@ public class Routes {
     public static final String PLAYLISTS="/playlists";
     public static final String PLAYLISTS1="/playlists123";
     public static final String PlaylistCHeck="Mahesh Done changes on remote file directly";
-    public static final String PlaylistCHeck="Harish Done changes on remote file directly";
+    public static final String PlaylistCHeck1="Harish Done changes on remote file directly";
 }
