42fe9d2382d20d35d7b9fa5734c558a92288dd0d
Monisha <chatwithmaheshmp@gmail.com>
2022-01-26 18:19:17 +0530

    updated by mahesh using jenkins

--- src/test/java/com/spotify/oauth2/utility/Routes.java
+++ src/test/java/com/spotify/oauth2/utility/Routes.java
@@ -9,5 +9,5 @@ public class Routes {
     public static final String PLAYLISTS1="/playlists123";
     public static final String PlaylistCHeck="Mahesh Done changes on remote file directly";
     public static final String PlaylistCHeck1="Harish Done changes on remote file directly";
-    public static final String PlaylistJenkins="Triggering the build by jenkins - Mahesh";
+    public static final String PlaylistJenkins="Triggering the build by jenkins - MaheshMP";
 }
